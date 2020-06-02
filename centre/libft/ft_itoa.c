1b37f071c6e1fa71d4f6f4d1565004fa3e71e8ed
Alexandre KRISSANE <akrissan@e3r11p1.42.fr>
2020-06-02 16:45:57 +0200

    ajout Norm - V-1.05

--- centre/libft/ft_itoa.c
+++ centre/libft/ft_itoa.c
@@ -1,31 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akrissan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/02 16:27:33 by akrissan          #+#    #+#             */
+/*   Updated: 2020/06/02 16:45:14 by akrissan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-static int	ft_sizetab(unsigned int numb)
+static int		ft_sizetab(unsigned int numb)
 {
 	int size;
 
 	size = 0;
-	while ( numb >= 10)
+	while (numb >= 10)
 	{
 		numb = numb / 10;
 		size++;
 	}
-	size ++;
+	size++;
 	return (size);
-
 }
 
-
-char *ft_itoa(int n)
+char			*ft_itoa(int n)
 {
 	unsigned int	numb;
-	char		*str;
-	int		size;
+	char			*str;
+	int				size;
 
 	size = 0;
 	if (n >= 0)
 		numb = n;
-	if(n < 0)
+	if (n < 0)
 	{
 		numb = n * -1;
 		size = 1;
@@ -36,12 +46,11 @@ char *ft_itoa(int n)
 	str[size] = '\0';
 	while (size--)
 	{
-		if(numb >= 0)
+		if (numb >= 0)
 			str[size] = (numb % 10) + '0';
 		if (size == 0 && n < 0)
 			str[size] = '-';
 		numb = numb / 10;
 	}
 	return (str);
-
 }
