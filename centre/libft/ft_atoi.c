9e9d6b31842aadad360cf218d749aaa9c992a8df
Alexandre KRISSANE <akrissan@e3r11p1.42.fr>
2020-06-02 16:24:12 +0200

    Ajout Norme - V-1.04

--- centre/libft/ft_atoi.c
+++ centre/libft/ft_atoi.c
@@ -1,26 +1,37 @@
-int ft_atoi(const char *str)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akrissan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/02 15:54:06 by akrissan          #+#    #+#             */
+/*   Updated: 2020/06/02 15:59:46 by akrissan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_atoi(const char *str)
 {
-	    int sign;
-	        int result;
+	int sign;
+	int	result;
 
-		sign = 1;
-		    result = 0;
-
-		    while ((*str >= 9 && *str <= 13) || *str == 32)
-			    str++;
-		        if (*str == '+' ||*str == '-')
-			{
-				if(*str == '-')
-					sign *= -1;
-				str++;
-			}
-			    while (*str >= '0' && *str <= '9')
-				        {
-						result = (result *10) + (*str - '0');
-						str++;
-						    }
-
-			    result = result * sign;
-
-			    return (result);
+	sign = 1;
+	result = 0;
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result = (result * 10) + (*str - '0');
+		str++;
+	}
+	result = result * sign;
+	return (result);
 }
--- centre/libft/ft_bzero.c
+++ centre/libft/ft_bzero.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akrissan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/02 16:00:24 by akrissan          #+#    #+#             */
+/*   Updated: 2020/06/02 16:02:22 by akrissan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
-void    ft_bzero(void *str, size_t n)
+
+void	ft_bzero(void *str, size_t n)
 {
-	    unsigned char *string;
+	unsigned char *string;
 
-	    string = (unsigned char*)str;
-	        while (n--)
-			    {
-				    *string = 0;
-				    string++;
-				        }
+	string = (unsigned char*)str;
+	while (n--)
+	{
+		*string = 0;
+		string++;
+	}
 }
-
