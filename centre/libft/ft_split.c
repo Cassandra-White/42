54d1d998bb805a45cb96cabc7ad987a64ecf4b48
Alexandre KRISSANE <akrissan@e3r3p21.42.fr>
2020-06-06 14:23:39 +0200

    Norm V-1.07

--- centre/libft/ft_split.c
+++ centre/libft/ft_split.c
@@ -6,15 +6,15 @@
 /*   By: akrissan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/06 14:02:22 by akrissan          #+#    #+#             */
-/*   Updated: 2020/06/06 14:06:49 by akrissan         ###   ########.fr       */
+/*   Updated: 2020/06/06 14:22:16 by akrissan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static  char	**ft_wordfree(char **tab, int i)
+static char		**ft_wordfree(char **tab, int i)
 {
-	int j;
+	int	j;
 
 	j = 0;
 	while (j < i)
@@ -26,7 +26,6 @@ static  char	**ft_wordfree(char **tab, int i)
 	return (NULL);
 }
 
-
 char			*ft_wordtab(const char *s, char c)
 {
 	char	*word;
@@ -35,10 +34,10 @@ char			*ft_wordtab(const char *s, char c)
 	i = 0;
 	while (s[i] && s[i] != c)
 		i++;
-	if (!(word = malloc(sizeof(*word) * i+1 )))
+	if (!(word = malloc(sizeof(*word) * i + 1)))
 	{
 		free(word);
-		return(NULL);
+		return (NULL);
 	}
 	i = 0;
 	while (s[i] && s[i] != c)
@@ -50,52 +49,49 @@ char			*ft_wordtab(const char *s, char c)
 	return (word);
 }
 
-
-
-int     ft_wordnbr(char const *s, char c)
+int				ft_wordnbr(char const *s, char c)
 {
 	int word;
 	int i;
 
 	i = 0;
 	word = 0;
-	while(s[i])
+	while (s[i])
 	{
-		while(s[i] && (s[i] == c)) 
+		while (s[i] && (s[i] == c))
 			i++;
-		while(s[i] && (s[i] != c))
-			i++; 
+		while (s[i] && (s[i] != c))
+			i++;
 		word++;
 	}
 	return (word);
 }
 
-char    **ft_split(char const *s, char c)
+char			**ft_split(char const *s, char c)
 {
-	char **tab;
-	int wordnbr;
-	int i;
+	char	**tab;
+	int		wordnbr;
+	int		i;
 
-	if(!s)
+	if (!s)
 		return (NULL);
 	i = 0;
 	wordnbr = ft_wordnbr(s, c);
-	if(!(tab = malloc(sizeof(*tab) * (wordnbr + 1))))
+	if (!(tab = malloc(sizeof(*tab) * (wordnbr + 1))))
 		return (NULL);
 	while (*s)
 	{
-		while(*s && *s == c)
+		while (*s && *s == c)
 			s++;
-		if(*s && *s != c)
+		if (*s && *s != c)
 		{
-			if(!(tab[i] = ft_wordtab(s, c)))
-				return(ft_wordfree(tab, i));
+			if (!(tab[i] = ft_wordtab(s, c)))
+				return (ft_wordfree(tab, i));
 			i++;
-			while(*s && *s != c)
+			while (*s && *s != c)
 				s++;
 		}
 	}
-	tab[i] = NULL;
+	ab[i] = NULL;
 	return (tab);
-
 }
